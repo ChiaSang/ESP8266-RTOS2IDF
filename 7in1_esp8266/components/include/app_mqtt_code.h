cd7eef7cb8aa66f59617682105f84d01903fe637
Victor <vic.zkj@gmail.com>
2022-03-10 20:27:28 +0800

    🦄 refactor: define sensor struct and function

--- 7in1_esp8266/components/include/app_mqtt_code.h
+++ 7in1_esp8266/components/include/app_mqtt_code.h
@@ -5,5 +5,17 @@
 #define ONENET_HOST "studio-mqtt.heclouds.com"
 #define ONENET_PORT 1883
 
-esp_err_t app_open_mqtt_connection(oneNET_connect_msg_t* oneNET_connect_msg);
+typedef struct S7IN1
+{
+    int CO2;
+    int CH2O;
+    int TVOC;
+    int PM25;
+    int PM10;
+    float Temp;
+    float Humi;
+} sensor_7in1_t;
+
+void app_start();
+
 #endif
